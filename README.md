baa28b6ceb47573c842c8109bf0bcf57094b5d81
Alex Fortuna <fortunadze@gmail.com>
2018-07-24 01:45:08 +0300

    Update
    * Implementation improvements.
    * README update.

--- README.md
+++ README.md
@@ -3,13 +3,11 @@ bundler-gemlocal
 
 Local Gemfile solution that works
 
-Overview
---------
+## Overview
 
 By using `bundler-gemlocal` developers can have their own custom Gemfile, called `Gemlocal` by default, in addition to team-wide `Gemfile`. The solution is easy to use and ensures no issues with `Gemfile.lock`.
 
-Setup
------
+## Setup
 
 1. Download `bundler-gemlocal.sh` into your home directory:
 
@@ -31,8 +29,7 @@ Setup
     function
     ```
 
-Usage
------
+## Usage
 
 * `b` is a Gemlocal-aware alias to `bundle`.
 * `bx` is a Gemlocal-aware alias to `bundle exec`.
@@ -44,11 +41,10 @@ $ b install --path vendor/bundle
 $ b check
 $ bx gem list -l
 $ bx rails console
-etc.
+…
 ```
 
-Project setup example
----------------------
+## Project setup example
 
 1. Cd to your project directory:
 
@@ -83,7 +79,7 @@ Project setup example
       gem "ori"
       gem "rdoc"
       gem "wirb"
-      # etc.
+      # …
     end
     ```
 
@@ -101,9 +97,7 @@ Project setup example
 
 7. **All done!** From now on, keep editing both `Gemfile` and `Gemlocal` as you like. Then do a `b install` and it'll sort everything out for you.
 
-
-Why not `Gemfile.local`?
-------------------------
+## Why not `Gemfile.local`?
 
 1. Because I like this:
 
@@ -128,11 +122,20 @@ Why not `Gemfile.local`?
     > Legend: (`o`: in the repo, `-`: not in the repo).
 
 2. Because "Gemlocal" is **both** a term and a filename, just like "Gemfile". It's more documentation-friendly.
-3. Because the blogs are full of half-working `Gemfile.local` recipes which will cause you nothing but pain. Find this page by the word "Gemlocal" and stay trouble-free!
+3. Because Internet is full of half-working `Gemfile.local` recipes that will cause you nothing but pain. Find this page by the word "Gemlocal" and stay trouble-free!
 
-Cheers!
--------
+## Example `Gemlocal` guidelines for a Rails project team
+
+1. Git should ignore `Gemlocal` and `Gemlocal.lock` in project root (see [Project setup example](#project-setup-example)).
+2. A basic `Gemlocal.example` with comments on how to enable it should be in Git repository.
+3. Team-wide `Gemfile` should contain no "developer comfort" gems (consoles, debuggers, profilers, &hellip;) or contain a bare, safe, time-proven, approved minimum set of 1-2 items. Adding more of such gems to `Gemfile` should be prohibited.
+4. Rails initializers (`config/initializers/*`) for "developer comfort" gems should gracefully handle the case when a specific gem is missing.
+   1. A simple yet effective way is to achieve it is via "example files". Say, `subsys.rb` should be ignored by Git, whereas `subsys.rb.example` should be in Git repository.
+5. It's highly desirable that `Gemlocal.example` lists, in a commented form, all "developer comfort" gems which are known to be used by particular team members.
+6. Project documentation should list Bundler commands in their canonical form: `bundle …`. Documentation sections covering developer tools should mention that the project is ready for `bundler-gemlocal` use.
+
+## Cheers!
 
 Feedback of any kind is highly appreciated.
 
-&mdash; Alex Fortuna, &copy; 2015-2017
+&mdash; Alex Fortuna, &copy; 2015-2018
--- bundler-gemlocal.sh
+++ bundler-gemlocal.sh
@@ -11,77 +11,76 @@
 # CONFIG: You can rename this.
 b()
 {
-  # Save `set` state. `eval "$SS"` restores it to original.
-  local SS=`shopt -op xtrace`; set +x; local RES
+  # Everything in a subshell.
+  (
+    set +x
 
-  # The filename for a Gemlocal.
-  # CONFIG: You can rename this.
-  local C_GEMLOCAL="Gemlocal"
+    # Always strict mode.
+    set -u -o pipefail
 
-  # If `BUNDLE_GEMFILE` is set, just run Bundler.
-  if [ -n "$BUNDLE_GEMFILE" ]; then
-    bundle "$@"; RES=$?
-    eval "$SS"
-    return $RES
-  fi
+    # The filename for a Gemlocal.
+    # CONFIG: You can rename this.
+    local C_GEMLOCAL="Gemlocal"
 
-  # Search for a Gemlocal and/or a Gemfile up the tree.
-  local D="$PWD"
-  while [ -n "$D" ]; do
-    if [ -e "$D/$C_GEMLOCAL" ]; then
-      local FOUND_GEMLOCAL="$D/$C_GEMLOCAL"
+    # If `BUNDLE_GEMFILE` is set, just run Bundler.
+    if [ -n "${BUNDLE_GEMFILE:-}" ]; then
+      bundle "$@"
+      return $?
     fi
 
-    if [ -e "$D/Gemfile" ]; then
-      local FOUND_GEMFILE="$D/Gemfile"
+    # Search for a Gemlocal and/or a Gemfile up the tree.
+    local FOUND_GEMFILE=
+    local FOUND_GEMLOCAL=
+    local D="$PWD"
+    while [ -n "$D" ]; do
+      if [ -e "$D/$C_GEMLOCAL" ]; then
+        local FOUND_GEMLOCAL="$D/$C_GEMLOCAL"
+      fi
+
+      if [ -e "$D/Gemfile" ]; then
+        local FOUND_GEMFILE="$D/Gemfile"
+      fi
+
+      [ -n "$FOUND_GEMLOCAL$FOUND_GEMFILE" ] && break
+
+      D=${D%/*}
+    done
+
+    if [ -n "$FOUND_GEMLOCAL" -a -z "$FOUND_GEMFILE" ]; then
+      echo "Error: No 'Gemfile' found next to '$FOUND_GEMLOCAL'" >&2
+      return 1
     fi
 
-    [ -n "$FOUND_GEMLOCAL$FOUND_GEMFILE" ] && break
+    # NOTE: This is the final command which sets the shell return result.
+    if [ -n "$FOUND_GEMLOCAL" ]; then
+      # Is it an install request?
+      # NOTE: Bundler supports abbreviated commands. Match a few here.
+      if [ "$1" = "" -o "$1" == "ins" -o "$1" == "inst" -o "$1" == "install" ]; then
+        # Run Bundler for Gemfile, then for Gemlocal.
+        bundle "$@" && {
+          # Create initial `Gemlocal.lock`.
+          if [ ! -f "${FOUND_GEMLOCAL}.lock" ]; then
+            cp -f "${FOUND_GEMFILE}.lock" "${FOUND_GEMLOCAL}.lock" 2>/dev/null
+          fi
 
-    D=${D%/*}
-  done
-
-  if [ -n "$FOUND_GEMLOCAL" -a -z "$FOUND_GEMFILE" ]; then
-    echo "Error: No 'Gemfile' found next to '$FOUND_GEMLOCAL'" >&2
-    eval "$SS"
-    return 1
-  fi
-
-  # NOTE: This is the final command which sets the shell return result.
-  if [ -n "$FOUND_GEMLOCAL" ]; then
-    # Is it an install request?
-    # NOTE: Bundler supports abbreviated commands. Match a few here.
-    if [ "$1" = "" -o "$1" == "ins" -o "$1" == "inst" -o "$1" == "install" ]; then
-      # Run Bundler for Gemfile, then for Gemlocal.
-      bundle "$@" && {
-        # Create initial `Gemlocal.lock`.
-        if [ ! -e "${FOUND_GEMLOCAL}.lock" ]; then
-          cp -f "${FOUND_GEMFILE}.lock" "${FOUND_GEMLOCAL}.lock"
-        fi
-
+          BUNDLE_GEMFILE="$FOUND_GEMLOCAL" bundle "$@"
+        }
+      else
+        # Run Bundler for Gemlocal only.
         BUNDLE_GEMFILE="$FOUND_GEMLOCAL" bundle "$@"
-      }
+      fi
     else
-      # Run Bundler for Gemlocal only.
-      BUNDLE_GEMFILE="$FOUND_GEMLOCAL" bundle "$@"
+      # No Gemlocal, just run Bundler.
+      bundle "$@"
     fi
-  else
-    # No Gemlocal, just run Bundler.
-    bundle "$@"
-  fi
-
-  RES=$?; eval "$SS"; return $RES
+  )
 }
 
 # Gemlocal-aware alias to `bundle exec`.
 # CONFIG: You can rename this.
 bx()
 {
-  local SS=`shopt -op xtrace`; set +x; local RES
-
-  b exec "$@"; RES=$?
-
-  eval "$SS"; return $RES
+  b exec "$@"
 }
 
 # Export commands to subshells and scripts. Bash-specific.
